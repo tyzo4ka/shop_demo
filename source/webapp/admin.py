1782a0c68d991fb523b0173a5acb358a2b579123
Farid <thesomeq@gmail.com>
2019-09-03 14:48:33 +0600

    Начальный коммит с моделью товара и тестовыми данными. Добавлены фикстуры, миграция, модель зарегистрирована в админ-сайте, настроена загрузка и отдача загруженных файлов.

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from webapp.models import Product
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'category', 'price')
+    list_filter = ('category',)
+
+
+admin.site.register(Product, ProductAdmin)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+CATEGORY_CHOICES = (
+    ('other', 'Другое'),
+    ('food', 'Еда'),
+    ('clothes', 'Одежда'),
+    ('household', 'Товары для дома'),
+)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Товар')
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, default=CATEGORY_CHOICES[0][0],
+                                verbose_name='Категория')
+    photo = models.ImageField(upload_to='product_images', null=True, blank=True, verbose_name='Фото')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
