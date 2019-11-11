dcfbee2c5d5ad7a6c3215e3259d7ba8580a76fbe
tyzo4ka <tyzo4ka@gmail.com>
2019-11-11 23:11:03 +0600

    typo fixed, extra code removed, output corrected

--- webapp/views.py
+++ webapp/views.py
@@ -10,10 +10,8 @@ from django.views.generic import ListView, DetailView, CreateView, TemplateView
 class StatisticMixin:
 
     def dispatch(self, request, *args, **kwargs):
-        # request.session = {"all_time": 0, "counter": 0, "pages": [], "index": {"pagetime": 0, "qt": 0}}
         timenow = datetime.now()
         timenow_str = timenow.__str__()
-        print("time now", timenow_str)
 
         if request.session.get("timenow") == None:
             request.session['timenow'] = timenow_str
@@ -24,7 +22,6 @@ class StatisticMixin:
             request.session['timenow'] = new_time.__str__()
             difference = new_time - last_time
             difference_str = difference.__str__()
-            print("difference", difference)
             if request.session.get("all_time") == None:
                 request.session['all_time'] = difference_str
             else:
@@ -39,93 +36,25 @@ class StatisticMixin:
             if 'pages' in request.session:
                 if request.path in request.session["pages"]:
                     request.session[request.path]['qt'] += 1
-                    if request.session[request.path].get("pagatime") is None:
-                        request.session[request.path]["pagatime"] = difference_str
+                    if request.session[request.path].get("pagetime") is None:
+                        request.session[request.path]["pagetime"] = difference_str
                     else:
-                        request.session[request.path]['pagetime'] = (datetime.strptime(request.session[request.path].get('pagetime'), "%Y-%m-%d %H:%M:%S.%f") +
-                         difference).__str__()
-
-                    # request.session[request.path]['pagetime'] = \
-                    #     (datetime.strptime(request.session[request.path].get('pagetime'), "%Y-%m-%d %H:%M:%S.%f") +
-                    #      difference).__str__()
+                        old_pagetime = request.session[request.path].get('pagetime')
+                        old_pagetime_data = datetime.strptime(old_pagetime, "%Y-%m-%d %H:%M:%S.%f")
+                        new_data = old_pagetime_data + timedelta(seconds=difference.total_seconds())
+                        request.session[request.path]['pagetime'] = new_data.__str__()
                 else:
                     request.session['pages'].append(request.path)
-                    # request.session[request.path] = {"pagetime": "0000-00-00 0:0:0.0", "qt": 0}
-                    request.session[request.path] = {"pagetime": None, "qt": 0}
+                    request.session[request.path] = {"qt": 0}
 
             else:
                 request.session['pages'] = []
-        print(request.session["pages"])
-        # print(request.session["all_time"])
-        print(request.session["counter"])
-        print(request.session["pages"])
         return super().dispatch(request, *args, **kwargs)
 
 
 class IndexView(StatisticMixin, ListView):
     model = Product
     template_name = 'index.html'
-
-    # def get(self, request, *args, **kwargs):
-    #     now = datetime.now()
-        # request.session['statistic'] = {"time": 0, "counter": 0, "pages": [], }
-        # d1 = datetime.datetime.now()
-        # dw = datetime.datetime.now()
-        # d3 = datetime.datetime.now()
-        # d_str = dw.__str__()
-        # d_fromstr = datetime.datetime.strptime(d_str, "%Y-%m-%d %H:%M:%S.%f")
-        # print(d3 - d_fromstr)
-        # now_str = now.__str__()
-        # now.__str__()
-        # # request.session['statistic'] = {}
-        # request.session["time"] = now_str
-        # after = datetime.now()
-        # time = request.session.get('time')
-        # print("time", time)
-        # time2 = datetime.strptime(time, "%Y-%m-%d %H:%M:%S.%f")
-        # difference = after - time2
-        # difference = timedelta(seconds=0) + difference
-        # request.session["difference"] = difference.__str__()
-        # print('difference', request.session.get("difference"))
-
-
-        # if 'counter' in request.session:
-        #     request.session["counter"] += 1
-        # else:
-        #     request.session["counter"] += 0
-        # print("counter", request.session.get('counter'))
-        # request.session["counter"] = 0
-        # request.session["counter"] += 1
-        # print(request.session.get('counter'))
-        # request.session["pages"] = []
-        # request.session["pages"].append(request.path)
-        # print(request.session.get('pages'))
-        # request.session[request.path] = {"pagetime": timedelta(seconds=0).__str__(), "qt": 0}
-        # print("jdjdjjdj", request.session[request.path].get("pagetime"))
-        # if request.path in request.session["pages"]:
-        #     request.session[request.path]['qt'] += 1
-            # request.session[request.path]['pagetime']
-            # page_time = datetime.now() - request.session["time"]
-
-        #
-        #     # request.session[request.path]['pagetime'] += page_time.total_seconds()
-        # else:
-        #     request.session[request.path] = {"pagetime": 0, "qt": 0}
-        # # print(request.session['time'])
-        # print(request.session['counter'])
-        # print(request.session['pages'])
-        # print(request.session['/'])
-
-
-        # dict = {"time": 0, "counter": 0, "pages": [], "index": {"pagetime" : 0, "qt" : 0}
-        # print(request.path)
-        # # print(now)
-        # print(request.session.values())
-        # for key, value in request.session.items:
-        #     print(key, value)
-        # print(dir(request.session))
-        # print(request.session.get("url"))
-        # return super().get(request, *args, **kwargs)
 
 
 class ProductView(DetailView):
@@ -221,15 +150,19 @@ class BasketView(CreateView):
 
 class StatisticView(StatisticMixin, View):
     def get(self, request, *args, **kwargs):
-        # request.session = {"all_time": 0, "counter": 0, "pages": [], "index": {"pagetime": 0, "qt": 0}}
         all_time = request.session.get("all_time")
         counter = request.session.get("counter")
-        pages = request.session.get("pages").set()
+        pages = set(request.session.get("pages"))
+        print("pages", pages)
         pages_statistic = {}
+
         for path in pages:
             pages_statistic[path] = {}
-            pages_statistic[path]["pagetime"] = request.session.get(path).get("pagetime")
+            print('request.session.get(path).get("pagetime")', request.session[path].get("pagetime"))
+            pages_statistic[path]["pagetime"] = request.session[path].get("pagetime")
             pages_statistic[path]["qt"] = request.session.get(path).get("qt")
+
+        print('pages_statistic', pages_statistic)
         return render(request, 'product/statistic.html', {'all_time': all_time,
                                                           "counter": counter,
                                                           "pages": pages,
